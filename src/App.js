7c6363f16a3785dd80f43834c7353a718463ccf2
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-07-24 10:15:38 +0500

    React Router with about page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/UI/Navbar';
 import Users from './Components/Users/Users';
 import Search from './Components/UI/Search';
 import Alert from './Components/UI/Alert';
+import About from './Components/pages/About';
 import axios from 'axios';
 
 // Class Based Components
@@ -19,12 +21,10 @@ class App extends Component {
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-
     this.setState({ users: res.data.items, loading: false });
   };
 
   clearUsers = () => this.setState({ users: [], loading: false });
-
   // Alerting
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
@@ -33,20 +33,34 @@ class App extends Component {
 
   render() {
     const { users, loading } = this.state;
+
     return (
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/Components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this Application</h1>
+      <p>Searching GitHub users by names</p>
+      <p>Version: 2.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
